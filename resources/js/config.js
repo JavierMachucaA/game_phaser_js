7050dd2ffca91a9f120bdd38315427651c0f7e80
JavierMachucaA <javier.machuca.a@gmail.com>
2019-10-20 23:49:21 -0300

    Se crea juego basico con phase

---
+++ resources/js/config.js
@@ -0,0 +1,20 @@
+
+import {preload, create, update} from './game.js'
+export const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+
+};
